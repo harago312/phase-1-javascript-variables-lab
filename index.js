a9c99eb9f3b28e641dcac186f5677765946bbde4
harago312 <ayisse312@gmail.com>
2022-11-24 21:13:46 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+/*let companyname = 'Scuber';
+console.log(companyname)*/
+const companyName = "Scuber";
+/*let companyname = 'Scuber';*/
+console.log(companyName)
+
+let mostProfitableNeighborhood = 'Chelsea';
+console.log(mostProfitableNeighborhood)
+
+let companyCeo= 'Susan Smith';
+console.log(companyCeo)
